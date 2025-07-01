03049eae57128791e5f9d07cb8f956810bd19601
mesoflix <209336738+mesoflix@users.noreply.github.com>
2025-07-01 01:37:10 -0700

    Update config.ts

--- packages/shared/src/utils/config/config.ts
+++ packages/shared/src/utils/config/config.ts
@@ -15,19 +15,19 @@ export const livechat_client_id = '66aa088aad5a414484c1fd1fa8a5ace7';
 
 export const domain_app_ids = {
     // these domains as supported "production domains"
-    'deriv.app': 16929, // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
-    'app.deriv.com': 16929,
-    'staging-app.deriv.com': 16303,
-    'app.deriv.me': 1411,
-    'staging-app.deriv.me': 1411, // TODO: setup staging for deriv.me
-    'app.deriv.be': 30767,
-    'staging-app.deriv.be': 31186,
+    'deriv.app': 82976, // TODO: [app-link-refactor] - Remove backwards compatibility for `deriv.app`
+    'app.deriv.com': 82976,
+    'staging-app.deriv.com': 82976,
+    'app.deriv.me': 82976,
+    'staging-app.deriv.me': 82976, // TODO: setup staging for deriv.me
+    'app.deriv.be': 82976,
+    'staging-app.deriv.be': 82976,
     'binary.com': 1,
-    'test-app.deriv.com': 51072,
+    'test-app.deriv.com': 82976,
 };
 
 export const platform_app_ids = {
-    derivgo: 23789,
+    derivgo: 82976,
 };
 
 export const getCurrentProductionDomain = () =>
